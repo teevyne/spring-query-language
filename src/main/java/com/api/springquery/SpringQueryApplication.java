55716af7e7e9b0dc1a72f3eb75bbe3207d675f67
Tolulope Ayemobola <ayemobolatolulope@gmail.com>
2022-04-07 14:01:21 +0100

    pushing code for spring query language

---
+++ src/main/java/com/api/springquery/SpringQueryApplication.java
@@ -0,0 +1,13 @@
+package com.api.springquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringQueryApplication.class, args);
+    }
+
+}
